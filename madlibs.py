38842db00c9a14c775d4057291e44d957108bbcb
wildanGhifari <wildghifari@gmail.com>
2021-02-24 21:37:41 +0700

    String concatenation

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+# String concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to ___"
+youtuber = "Kylie Ying & freeCodeCamp.org"  # some string variable
+
+# a few ways to do this
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
