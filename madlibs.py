2aa0cb7ac3df1a61a51420ca7331e6f2f47b9733
wildanGhifari <wildghifari@gmail.com>
2021-02-24 21:47:22 +0700

    Simple madlibs, done

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,18 @@
 # String concatenation (aka how to put strings together)
 # suppose we want to create a string that says "subscribe to ___"
-youtuber = "Kylie Ying & freeCodeCamp.org"  # some string variable
+# youtuber = "Kylie Ying & freeCodeCamp.org"  # some string variable
 
-# a few ways to do this
-print("subscribe to " + youtuber)
-print("subscribe to {}".format(youtuber))
-print(f"subscribe to {youtuber}")
+# # a few ways to do this
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
